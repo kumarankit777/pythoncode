916e33f9f385c13df2015fce23bd6011dc02e178
Ankit kumar <40390275+kumarankit777@users.noreply.github.com>
2018-06-26 21:28:37 +0530

    Add files via upload

---
+++ camera_take.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import cv2
+
+#to start web or external camera
+
+capture=cv2.VideoCapture(0)
+
+
+if capture.isOpened() :
+    print "camera is ready to take pictures"
+#   current camera data,after frame take camera status
+    frame,status=capture.read()
+    cv2.imshow("framm1",frame)
+    cv2.waitKey(0)
+    capture.release()
+
+else :
+    print"check your camera drivers with OS"
+
